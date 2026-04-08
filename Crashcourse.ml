c501ad74d869816a8d7b61bf9298c9e86b8c97bb
Charles Hamilton <crh2635@gmail.com>
2026-04-07 22:18:56 -0500

    first mac branch commit

--- Crashcourse.ml
+++ Crashcourse.ml
@@ -1,3 +1,14 @@
+(*Notes adapted from XVWs crash course*)
+
+
+(*Objects in other languages*)
+
+(*Objects differ from modules because of type system*)
+
+
+(*modules remain within the ML type systems*)
+
+(*module AST*)
 module W : sig 
     type a = string
     val f: a -> unit 
@@ -9,9 +20,10 @@ end
 module type Mylist 
 
 
-
+(*Functor AST*)
 module Anon = functor (_:Y) -> struct end 
 
 module type Anon2 = functor F (_:_) -> sig end 
 
 let () = "invalid"
+(*Coroners of the module system*)
